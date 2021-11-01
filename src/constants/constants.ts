850ade4196877059305108776e5552a6b0a53ea6
aggre <hiroyuki.aggre@gmail.com>
2021-11-01 18:08:26 +0900

    fill up addresses

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,8 +1,8 @@
 import { AvailableNetwork, L1Network } from '../types/types';
 
 // !!! TODO these need to be updated to correct bridge contracts
-export const MAINNET_GATEWAY_ADDRESS = '';
-export const ARBITRUM_MAINNET_GATEWAY_ADDRESS = '';
+export const MAINNET_GATEWAY_ADDRESS = '0x72Ce9c846789fdB6fC1f34aC4AD25Dd9ef7031ef';
+export const ARBITRUM_MAINNET_GATEWAY_ADDRESS = '0xcEe284F754E854890e311e3280b767F80797180d';
 export const RINKEBY_GATEWAY_ADDRESS = '0x917dc9a69F65dC3082D518192cd3725E1Fa96cA2';
 // export const RINKEBY_GATEWAY_ADDRESS = '0x91169Dbb45e6804743F94609De50D511C437572E';
 export const ARBITRUM_RINKEBY_GATEWAY_ADDRESS = '0x9b014455AcC2Fe90c52803849d0002aeEC184a06';
@@ -11,10 +11,10 @@ export const ARBITRUM_RINKEBY_GATEWAY_ADDRESS = '0x9b014455AcC2Fe90c52803849d000
 
 export const L1_MAINNET_DEV_ADDRESS = '0x5caf454ba92e6f2c929df14667ee360ed9fd5b26';
 export const L1_RINKEBY_DEV_ADDRESS = '0xa21cb351fc29acb7c3901270a5259bf5e68f11d8'; // TODO: this needs to be changed is Dummy DEV for testing
-export const ARB_MAINNET_DEV_ADDRESS = '';
+export const ARB_MAINNET_DEV_ADDRESS = '0x91F5dC90979b058eBA3be6B7B7e523df7e84e137';
 export const ARB_RINKEBY_DEV_ADDRESS = '0x53a8fc7d1663807eac3daafa81b5b3c81f028479'; // TODO: this needs to be changed is Dummy DEV for testing
 export const ARB_DEV_WRAPPER_RINKEBY = '0xb970C9AB82C9b5110d734bA413AA3527ddd9eB6F';
-export const ARB_DEV_WRAPPER_MAINNET = '';
+export const ARB_DEV_WRAPPER_MAINNET = '0xcc069f8323858537158Dc47F6c208C8174038aF9';
 
 // TODO - separate out available BRIDGE networks and available WRAP (only L1) networks
 
