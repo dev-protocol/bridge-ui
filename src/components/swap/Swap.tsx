c4a583f8cd2f129277ce172081406a8147394a4a
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2023-04-12 11:05:31 +0900

    craco update for uniswap widget

--- src/components/swap/Swap.tsx
+++ src/components/swap/Swap.tsx
@@ -1,22 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import { SwapWidget } from '@uniswap/widgets';
-import { useWeb3Provider } from '../../context/web3ProviderContext';
-import { UndefinedOr } from '@devprotocol/util-ts';
-import { providers } from 'ethers';
+import React from 'react';
+// import { SwapWidget } from '@uniswap/widgets';
+// import '@uniswap/widgets/fonts.css';
+
+import { SwapWidget } from '@uniswap/widgets/dist/index.js';
+import '@uniswap/widgets/dist/fonts.css';
 
 const SwapComponent: React.FC = () => {
-	const web3Context = useWeb3Provider();
-	const [currentProvider, setCurrentProvider] = useState<UndefinedOr<providers.Provider>>();
-
-	useEffect(() => {
-		if (web3Context?.web3Provider) {
-			setCurrentProvider(web3Context?.web3Provider);
-		}
-	}, [web3Context?.web3Provider]);
-
 	return (
 		<div className="flex flex-col items-center justify-center w-full h-full">
-			<SwapWidget provider={currentProvider ?? undefined} jsonRpcUrlMap={[]} />
+			<div className="Uniswap">
+				<SwapWidget />
+			</div>
 		</div>
 	);
 };
